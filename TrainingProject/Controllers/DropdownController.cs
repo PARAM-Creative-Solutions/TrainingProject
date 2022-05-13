fe580c31602e7a5bb080ae1343c002a70c58b759
Vikrant Kate <vikrant.kate@paramcs.com>
2022-05-13 22:09:07 +0530

    Dropdown related changes added.

--- TrainingProject/Controllers/DropdownController.cs
+++ TrainingProject/Controllers/DropdownController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrainingProject.ViewModels.Dropdown;
+using TrainingProjectDataLayer.DataLayer.Unit_of_Work.Implementation;
 
 namespace TrainingProject.Controllers
 {
-    public class DropdownController : Controller
+    public class DropdownController : BaseController
     {
+        public DropdownController()
+        {
+            uow = new UnitofWork(new TrainingProjectDataLayer.DataLayer.Entities.DAL.TrainingProjectEntities());
+        }
+
+
         // GET: Dropdown
         public ActionResult Index()
         {
-            return View();
+            DropdownViewModel model = new DropdownViewModel();
+            ViewBag.UsersConstructor1 = new SelectList(uow.UserRepository.GetAll().Select(s=>s.UserName));
+
+            ViewBag.UsersConstructor2 = new SelectList(uow.UserRepository.GetAll().Select(w=>w.UserName), "PPDAdmin");
+
+            List<string> disabledValue = new List<string> { "PPDAdmin", "sysadmin" };
+            ViewBag.UsersConstructor3 = new SelectList(uow.UserRepository.GetAll().Select(w=>w.UserName), "EPDAdmin", disabledValue);
+
+
+            //ViewBag.UsersConstructor1 = new SelectList(uow.UserRepository.GetAll());
+            //ViewBag.UsersConstructor1 = new SelectList(uow.UserRepository.GetAll());
+            //ViewBag.UsersConstructor1 = new SelectList(uow.UserRepository.GetAll());
+            //ViewBag.UsersConstructor1 = new SelectList(uow.UserRepository.GetAll());
+            return View(model);
         }
     }
 }
--- TrainingProject/ViewModels/Dropdown/DropdownViewModel.cs
+++ TrainingProject/ViewModels/Dropdown/DropdownViewModel.cs
@@ -7,5 +7,15 @@ namespace TrainingProject.ViewModels.Dropdown
 {
     public class DropdownViewModel
     {
+
+        public string User { get; set; }
+
+        public string[] Users { get; set; }
+
+        public int UserId { get; set; }
+
+        public int[] UserIds { get; set; }
+
+
     }
 }
